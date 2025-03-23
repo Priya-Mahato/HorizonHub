c479b95e115afe93a53ba334297d0a32c9f58c5a
Priya Mahato <priyamahato1950@gmail.com>
2025-03-24 01:10:14 +0530

    Heading edited

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -21,7 +21,7 @@ export default function WeatherApp() {
 
     return (
         <div className="WeatherApp">
-            <h2>Weather Widget</h2>
+            <h2>HorizonHub</h2>
             <SearchBox updateInfo={updateInfo}/>
             <InfoBox info={weatherInfo}/>
         </div>
